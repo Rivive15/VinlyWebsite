b52a8f373a50dc788785d4f3cfa52a7508c4c5af
Rivive15 <christophergalinski15@gmail.com>
2023-12-01 11:58:30 +0000

    added stuff

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 let vinyls = [
 //vinyl 1
 {
-    "vinylimg":``,
-    "vinylname": "Simpiternal",
+    "vinylname": "Simpiternal - Bring Me The Horizon",
     "vinyldescription": String.raw`Fourth studio album by the British metalcore band Bring Me the Horizon. The album features three singles, 'Shadow Moses', 'Sleepwalking' and 'And the Snakes Start to Sing'. The album, the title of which means 'everlasting life', entered the UK Albums Chart at #3.`, 
     
     "spotifysrc": `https://open.spotify.com/embed/album/6IYPmM3xsOPL2XPSvf1ZAz?utm_source=generator&theme=0`,
 },
 //vinyl 2
 {
-    "vinylname": "POST HUMAN:SURVIVAL HORROR",
+    "vinylname": "POST HUMAN:SURVIVAL HORROR - Bring Me The Horizon",
     "vinyldescription": String.raw`Continuing to push their creative boundaries, even during lockdown, the band have created a body of work that is arguably one of their most exciting, diverse, intricate pieces of music to date. The first two tracks from the EP 'Parasite Eve' and the recent colossus 'Obey' featuring fellow Brit YUNGBLUD, have received critical acclaim and amassed over 115 Million audio and video streams combined to date. Other guests on the EP include BABYMETAL, Nova Twins and Amy Lee from Evanescence.`,
     "spotifysrc": `https://open.spotify.com/embed/album/0e1WaSNDZnoPixaxDNdWo4?utm_source=generator`,
 },
 //vinyl 3
 {
-    "vinylname": "The New Abnormal",
+    "vinylname": "The New Abnormal - The Strokes",
     "vinyldescription": String.raw`The New Abnormal is the sixth studio album by American rock band the Strokes, released on April 10, 2020, through Cult and RCA Records. 
     It was their first full-length album since Comedown Machine (2013), marking the longest gap between studio albums by the band. 
     The album was produced by Rick Rubin and recorded at his Shangri-La studio in Malibu, California, with additional recording taking place at studios in Los Angeles County and Hawaii. 
@@ -29,117 +28,117 @@ let vinyls = [
 },
 //vinyl 4
 {
-    "vinylname": "Swimming",
+    "vinylname": "Swimming - Mac Miller",
     "vinyldescription": "The fifth studio album by American rapper and singer, and the last album to be released during his lifetime. Production on the album was handled by Miller himself, as well as Dev Hynes, J. Cole, Dâm-Funk, DJ Dahi, Tae Beast, Flying Lotus, and Cardo, among others. The album has no credited features, but contains vocal contributions from Dâm-Funk, Dev Hynes, Snoop Dogg, Syd, Thundercat, and JID. Features the singles 'Small Worlds', 'Self Care' and 'What's the Use?'.",
     "spotifysrc": String.raw`https://open.spotify.com/embed/album/5wtE5aLX5r7jOosmPhJhhk?utm_source=generator`,
 },
 //vinyl 5
 {
-    "vinylname": "Circles",
+    "vinylname": "Circles - Mac Miller",
     "vinyldescription": "'Circles' is the sixth and final studio album by Mac Miller. Conceived as a sister album to 2018's full length, 'Swimming'. While sonically distinctly different than its predecessor, 'Circles' features many of the hallmarks for which 'Swimming' was critically-acclaimed upon its release - Miller further realising his singing voice in addition to rapping, live instrumentation and earnest, confessional lyrical content.",
     "spotifysrc": String.raw`https://open.spotify.com/embed/album/5sY6UIQ32GqwMLAfSNEaXb?utm_source=generator`,
 },
 //vinyl 6
 {
-    "vinylname": "Actual Life 3",
-    "vinyldescription": "",
+    "vinylname": "Actual Life 3 - Fred Again",
+    "vinyldescription": "Fred again.. returns on 28th October with the third in his series of Actual Life albums 'Actual Life 3 (January 1 - September 9 2022)'.",
     "spotifysrc": String.raw`https://open.spotify.com/embed/album/5q2iMctlDvEMYVIawF6Vop?utm_source=generator`,
 },
 //vinyl 7
 {
-    "vinylname": "",
-    "vinyldescription": "",
-    "spotifysrc": String.raw``,
+    "vinylname": "Faces - Mac Miller",
+    "vinyldescription": `Mac Miller's cult favourite mixtape Faces is finally available on triple vinyl. The project, which originally debuted on Mother's Day 2014, is a vital release in Mac's catalogue that followed-up his critically-acclaimed sophomore album Watching Movies With The Sound Off. Upon its arrival, the mixtape served as the high water mark for Mac's technical prowess and cemented a creative actualization that would continue through his career; reinventing himself with each new project. Reviewing the project following its release, critic Craig Jenkins succinctly described it as "the best work of his career.`,
+    "spotifysrc": String.raw`https://open.spotify.com/embed/album/5SKnXCvB4fcGSZu32o3LRY?utm_source=generator`,
 },
 //vinyl 8
 {
-    "vinylname": "",
-    "vinyldescription": "",
-    "spotifysrc": String.raw``,
+    "vinylname": "Is This It - The Strokes",
+    "vinyldescription": "The American rock band's critically acclaimed debut studio album. Building on their 2001 EP 'The Modern Age', the band members moulded compositions largely through live takes during recording sessions, while songwriter and lead singer Julian Casablancas continued to detail the lives and relationships of urban youth. Three singles were released from the album: 'Hard to Explain'/'New York City Cops', 'Last Nite', and 'Someday'.",
+    "spotifysrc": String.raw`https://open.spotify.com/embed/album/2yNaksHgeMQM9Quse463b5?utm_source=generator`,
 },
 //vinyl 9
 {
-    "vinylname": "",
-    "vinyldescription": "",
-    "spotifysrc": String.raw``,
+    "vinylname": "Angles - The Strokes",
+    "vinyldescription": `There's seldom a need to use the phrase return to form if you never really drop off, but The Strokes found themselves in a different spot for 2011's Angles, ten years since their rapid ascent and equally sudden plateau.A public re-assessment of sorts then, and one that found them in a rich vein of songwriting form - tantilizingly close to the level of Is This It. Highlights aplenty then - fan favourite Machu Pichu rubs shoulders with killer singles Taken for A Fool and Under Cover Of Darkness but there's so much to love about this record it's almost churlish to pick out highlights.`,
+    "spotifysrc": String.raw`https://open.spotify.com/embed/album/6Jx4cGhWHewTcfKDJKguBQ?utm_source=generator`,
 },
 //vinyl 10
 {
-    "vinylname": "",
-    "vinyldescription": "",
-    "spotifysrc": String.raw``,
+    "vinylname": "Room On Fire - The Strokes",
+    "vinyldescription": `The Strokes second full-length release was an album that won't cause the stir that its predecessor did, but has a sneaky appeal all its own. Thanks to the quintet's Lower East Side roots, Velvet Underground and Television references abound with these guys, but Boston new wavers the Cars, and in particular their hit-heavy second album, 1979's Candy-O, provide a more suitable point of reference for Room On Fire.As with Ric Ocasek and company, Strokes frontman Julian Casablancas and his cohorts have a Cars-like knack for sly riffs that creep deeper into ones consciousness with each listen. Not much longer than a half hour from start to finish, this 11-song is modest in intent and execution, and succeeds quite nicely on its own terms.The album features the return of producer Gordon Raphael from 'Is This It' and consists of a slightly smoother sound than its predecessor. Three singles were released from the album; '12:51', 'Reptilia' and 'The End Has No End'.`,
+    "spotifysrc": String.raw`https://open.spotify.com/embed/album/3HFbH1loOUbqCyPsLuHLLh?utm_source=generator`,
 },
 //vinyl 11
 {
-    "vinylname": "",
-    "vinyldescription": "",
-    "spotifysrc": String.raw``,
+    "vinylname": "Pilot - Hot Mulligan",
+    "vinyldescription": `Hot Mulligan writes songs for people to sing as loud as possible. Their music is the cathartic outcry for growth from a generation of forward-thinking Midwesterners caught in the gears of a rusted system in desperate need of hope. The members - Tades Sanville, Chris Freeman, Ryan Malicsi, Garrett "Sniff" Willig, and Brandon Blakeley - use songwriting to explore the lessons they've learned from lives lead in the pursuit of dreams with full awareness of the cost.`,
+    "spotifysrc": String.raw`https://open.spotify.com/embed/album/4LR74198fNxGkLyzinQwJz?utm_source=generator`,
 },
 //vinyl 12
 {
-    "vinylname": "",
-    "vinyldescription": "",
-    "spotifysrc": String.raw``,
+    "vinylname": "Opportunities - Hot Mulligan",
+    "vinyldescription": `Re-release of the excellent 2017 EP from Hot Mulligan . If you're into the likes of The Hotelier, Spanish Love Songs, Tiny Moving Parts or Free Throw should definitely check this out.`,
+    "spotifysrc": String.raw`https://open.spotify.com/embed/album/5WDg2HWBwm6W0jttEW2jXG?utm_source=generator`,
 },
 //vinyl 13
 {
-    "vinylname": "",
-    "vinyldescription": "",
-    "spotifysrc": String.raw``,
+    "vinylname": "You'll Be Fine - Hot Mulligan",
+    "vinyldescription": `The new full length album from the awesome emo-rockers Hot Mulligan, following on from their 2018 acclaimed record Pilot. If you're into the likes of The Hotelier, Spanish Love Songs, Tiny Moving Parts or Free Throw should definitely check this out. Hot Mulligan writes songs for people to sing as loud as possible. Their music is the cathartic outcry for growth from a generation of forward-thinking Midwesterners caught in the gears of a rusted system in desperate need ofhope. The members - Tades Sanville, Chris Freeman, Ryan Malicsi, Garrett “Sniff” Willig, and Brandon Blakeley - use songwriting to explore the lessons they’ve learned from lives lead in the pursuit of dreams with full awareness of the cost.`,
+    "spotifysrc": String.raw`https://open.spotify.com/embed/album/5MutKcn3J1xzbBKOzr1oGI?utm_source=generator`,
 },
 //vinyl 14
 {
-    "vinylname": "",
-    "vinyldescription": "",
-    "spotifysrc": String.raw``,
+    "vinylname": "I Won't Reach Out To You - Hot Mulligan",
+    "vinyldescription": `A new 5-track EP from one of our favourite pop-punk / emo-rock bands, Hot Mulligan! following on from the excellent 2020 album You'll Be Fine. If you're into the likes of The Hotelier, Spanish Love Songs, Tiny Moving Parts or Free Throw then you should definitely check this out.`,
+    "spotifysrc": String.raw`https://open.spotify.com/embed/album/5xSwJaRKEx137KQmidxkw6?utm_source=generator`,
 },
 //vinyl 15
 {
-    "vinylname": "",
-    "vinyldescription": "",
-    "spotifysrc": String.raw``,
+    "vinylname": "Those Days Are Gone - Free Throw",
+    "vinyldescription": `Those Days Are Gone is Nashville's emo-punk band Free Throw's debut, most popular, and fan-beloved album. Free Throw have been making music together for 10 years, and on Oct 13th, 2023, are releasing their fifth studio album to much fan and critical excitement. Those Days Are Gone was the last record that singer Cory Castro and drummer Zach Hall played on together before reuniting for the 2023 record. This 2023 release will surely drive fan excitement for Free Throw's entire discography, particularly favourite Those Days Are Gone. Free Throw is embarking on a full 30-date US tour in fall 2023, co-headline with Prince Daddy & The Hyena. They are also supporting Hot Mulligan on a 6-date Australia tour in early 2024.`,
+    "spotifysrc": String.raw`https://open.spotify.com/embed/album/0yhwDk0EqhcbTE5k0cqs1K?utm_source=generator`,
 },
 //vinyl 16
 {
-    "vinylname": "",
-    "vinyldescription": "",
-    "spotifysrc": String.raw``,
+    "vinylname": "White Pony - Deftones",
+    "vinyldescription": `Hot off the heels of their Top Selling New Album 'Ohms' in September, Deftones release the 20th Anniversary Deluxe Edition of their seminal album 'White Pony'. 20 years on from its first release, the set will include the original 'White Pony' album along with a companion remix album entitled 'Black Stallion'. This was an idea conceived during the original album's creation but didn't come to fruition at the time. 'Black Stallion' includes remixes of each of the individual 11 tracks from the original'White Pony' album by different artists including DJ Shadow, Robert Smith, Squarepusher and Mike Shinoda.`,
+    "spotifysrc": String.raw`https://open.spotify.com/embed/album/5LEXck3kfixFaA3CqVE7bC?utm_source=generator`,
 },
 //vinyl 17
 {
-    "vinylname": "",
-    "vinyldescription": "",
-    "spotifysrc": String.raw``,
+    "vinylname": "Koi No Yokan - Deftones",
+    "vinyldescription": `The seventh studio album by the American alternative metal band, originally released in November 2012. Its title is a phrase from the Japanese language, translating to 'premonition of love'. Band frontman Chino Moreno characterized the album as 'dynamic' with a full range of noise, noting an increased contribution of ideas by bassist Sergio Vega compared to their previous record, 'Diamond Eyes' (2010). Features the singles 'Leathers', 'Tempest', 'Swerve City' and 'Romantic Dreams'.`,
+    "spotifysrc": String.raw`https://open.spotify.com/embed/album/4PIVdqvL1Rc7T7Vfsr8n8Q?utm_source=generator`,
 },
 //vinyl 18
 {
-    "vinylname": "",
-    "vinyldescription": "",
-    "spotifysrc": String.raw``,
+    "vinylname": "Around The Fur - Deftones",
+    "vinyldescription": `The second studio album by the American alternative metal band, and the second album to feature Frank Delgado under additional personnel; he eventually joined the band officially in 1999. The song 'Headup' features additional vocals by Max Cavalera of Soulfly. It was written by Cavalera and Deftones singer Chino Moreno as a way of venting some of their pain over the loss of Dana Wells, Cavalera's stepson and Chino's friend. Features the singles 'My Own Summer (Shove It)' and 'Be Quiet and Drive (Far Away)'.`,
+    "spotifysrc": String.raw`https://open.spotify.com/embed/album/7o4UsmV37Sg5It2Eb7vHzu?utm_source=generator`,
 },
 //vinyl 19
 {
-    "vinylname": "",
+    "vinylname": "Diamond Eyes - Deftones",
     "vinyldescription": "",
-    "spotifysrc": String.raw``,
+    "spotifysrc": String.raw`https://open.spotify.com/embed/album/1GjjBpY2iDwSQs5bykQI5e?utm_source=generator`,
 },
 //vinyl 20
 {
-    "vinylname": "",
-    "vinyldescription": "",
-    "spotifysrc": String.raw``,
+    "vinylname": "Now That's What I Call Music Vol.420 - Mom Jeans",
+    "vinyldescription": `Now That's What I Call Music Vol. 420, a split release between Mom Jeans, Prince Daddy & The Hyena, and Pictures of Vernon, captures each band at a moment in time where they, along with Counter Intuitive Records itself, were in the midst of creating a new wave of emo infused pop punk. Originally released in 2017, the EP sees each band growing in new directions following now-classic releases and hinting at the great heights each would go on to reach in the following years. `,
+    "spotifysrc": String.raw`https://open.spotify.com/embed/album/0kxrhUHeJB2LUmk6zelTPu?utm_source=generator`,
 },
 //vinyl 21
 {
-    "vinylname": "",
-    "vinyldescription": "",
-    "spotifysrc": String.raw``,
+    "vinylname": "Sweet Tooth - Mom Jeans",
+    "vinyldescription": `Sweet Tooth, which arrives three years after Mom Jeans' second LP Puppy Love, was recorded in New Jersey at The Barber Shop Studios with producer Brett Romnes (The Front Bottoms, Oso Oso, Dogleg). Butler says it's the first time the band had a proper, top-to-bottom studio experience, which meant no cutting corners, and an ability to chase a previously-unattainable level of polish. Tyler Povanda (Save Face) and Kory Gregory (Prince Daddy & the Hyena) contributed vocals to the record. The music on Sweet Tooth marked a departure for Butler from his usual writing style. "I fell in love with bands like Modern Baseball and Front Bottoms and Joyce Manor, so I wanted to write songs like that," says Butler. "That's what our first two records were: me trying to stay in that box, cause I think that's where I felt comfortable." Sweet Tooth feels like an eternal dessert-before-dinner, a rejection of the idea that we need to suffer before we get to the good stuff. You've got one life. Indulge your Sweet Tooth. `,
+    "spotifysrc": String.raw`https://open.spotify.com/embed/album/2vMWoMKeyq9m1xIghMKkaL?utm_source=generator`,
 },
 //vinyl 22
 {
-    "vinylname": "",
-    "vinyldescription": "",
-    "spotifysrc": String.raw``,
+    "vinylname": "Best Buds - Mom Jeans",
+    "vinyldescription": `What Mom Jeans. have proved themselves capable of in the past 2 years is nothing short of fantastic. Growing from ordinary college students to national touring artists in a matter of months, the indie rock quartet has been on a trajectory most bands could only dream of. The DIY touring route quickly had to fall by the wayside, as 200, sometimes 300, kids were showing up to house shows in basements across the country. Often, the band wouldn’t make it through more than a few songs before police had to come shut it down. Now, you can find them in venues of increasing size around the states, as one of the most sought after acts in their genre. On a constant stream of buzz, pressing after pressing of their debut hit Best Buds (2016) has sold out and the fans cannot get enough of the music they have been putting out. Music that takes influence from predecessors such as Modern Baseball and The Front Bottoms, but adds that new flavor that sets them into a league of their own, The Mom Jeans. craze is just beginning.`,
+    "spotifysrc": String.raw`https://open.spotify.com/embed/album/0KzElSxcfAvleUIuOg0TOQ?utm_source=generator`,
 }
 ]
 
